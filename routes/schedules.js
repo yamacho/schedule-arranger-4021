fbdd55b3cda5576e9f2d40aba10ab0c0a8b27f40
Yuya Takashina <yuya_takashina@dwango.co.jp>
2016-06-02 16:19:40 +0900

    let to const

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,21 +1,21 @@
 'use strict';
-let express = require('express');
-let router = express.Router();
-let authenticationEnsurer = require('./authentication-ensurer');
-let uuid = require('node-uuid');
-let Schedule = require('../models/schedule');
-let Candidate = require('../models/candidate');
-let User = require('../models/user');
-let Availability = require('../models/availability');
-let Comment = require('../models/comment');
+const express = require('express');
+const router = express.Router();
+const authenticationEnsurer = require('./authentication-ensurer');
+const uuid = require('node-uuid');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
+const User = require('../models/user');
+const Availability = require('../models/availability');
+const Comment = require('../models/comment');
 
 router.get('/new', authenticationEnsurer, (req, res, next) => {
   res.render('new', { user: req.user });
 });
 
 router.post('/', authenticationEnsurer, (req, res, next) => {
-  let scheduleId = uuid.v4();
-  let updatedAt = new Date();
+  const scheduleId = uuid.v4();
+  const updatedAt = new Date();
   Schedule.create({
     scheduleId: scheduleId,
     scheduleName: req.body.scheduleName.slice(0, 255),
@@ -23,8 +23,8 @@ router.post('/', authenticationEnsurer, (req, res, next) => {
     createdBy: req.user.id,
     updatedAt: updatedAt
   }).then((schedule) => {
-    let candidateNames = req.body.candidates.trim().split('\n').map((s) => s.trim());
-    let candidates = candidateNames.map((c) => { return {
+    const candidateNames = req.body.candidates.trim().split('\n').map((s) => s.trim());
+    const candidates = candidateNames.map((c) => { return {
       candidateName: c,
       scheduleId: schedule.scheduleId
     };});
@@ -55,7 +55,7 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
         order: '"candidateId" ASC'
       });
     } else {
-      let err = new Error('指定された予定は見つかりません');
+      const err = new Error('指定された予定は見つかりません');
       err.status = 404;
       next(err);
     }
@@ -74,16 +74,15 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
     });
   }).then((availabilities) => {
     // 出欠 MapMap(キー:ユーザー ID, 値:出欠Map(キー:候補 ID, 値:出欠)) を作成する
-    let availabilityMapMap = new Map(); // key: userId, value: Map(key: candidateId, availability)
+    const availabilityMapMap = new Map(); // key: userId, value: Map(key: candidateId, availability)
     availabilities.forEach((a) => {
-      let map = availabilityMapMap.get(a.user.userId);
-      map = map ? map : new Map();
+      const map = availabilityMapMap.get(a.user.userId) || new Map();
       map.set(a.candidateId, a.availability);
       availabilityMapMap.set(a.user.userId, map);
     });
 
     // 閲覧ユーザーと出欠に紐づくユーザーからユーザー Map (キー:ユーザー ID, 値:ユーザー) を作る
-    let userMap = new Map(); // key: userId, value: User
+    const userMap = new Map(); // key: userId, value: User
     userMap.set(parseInt(req.user.id), {
       isSelf: true,
       userId: parseInt(req.user.id),
@@ -98,13 +97,11 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
     });
 
     // 全ユーザー、全候補で二重ループしてそれぞれの出欠の値がない場合には、「欠席」を設定する
-    let users = Array.from(userMap).map((keyValue) => keyValue[1]);
+    const users = Array.from(userMap).map((keyValue) => keyValue[1]);
     users.forEach((u) => {
       storedCandidates.forEach((c) => {
-        let map = availabilityMapMap.get(u.userId);
-        map = map ? map : new Map();
-        let a = map.get(c.candidateId);
-        a = a ? a : 0; // デフォルト値は 0 を利用
+        const map = availabilityMapMap.get(u.userId) || new Map();
+        const a = map.get(c.candidateId) || 0; // デフォルト値は 0 を利用
         map.set(c.candidateId, a);
         availabilityMapMap.set(u.userId, map);
       });
@@ -114,7 +111,7 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
     return Comment.findAll({
       where: { scheduleId: storedSchedule.scheduleId }
     }).then((comments) => {
-      let commentMap = new Map();  // key: userId, value: comment
+      const commentMap = new Map();  // key: userId, value: comment
       comments.forEach((comment) => {
         commentMap.set(comment.userId, comment.comment);
       });
